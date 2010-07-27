4153e33bcd8e6aea6769a3dfa32ebc56aed02a0a
Gregory Jefferis <jefferis@gmail.com>
2010-07-27 20:40:06 +0100

    Added function to read log files produced by Jason Rothman's Nclamp acqusition package
    # 2009-07-09
    # Fixed handling of dates in ReadNclampLogTable

---
+++ ReadNclamp.R
@@ -0,0 +1,46 @@
+# A log table is a special table produced by Nclamp
+# It consist of a PXP file with variables only
+ReadNclampLogTable<-function(f,Verbose=FALSE){
+	x=ReadIgorPackedExperiment(f,Verbose=Verbose)
+	
+#	WaveFields	
+	# Select text fields only
+	# cat("orig date field is:",x$vars$FileDate)
+	exptDate=strptime(x$vars$FileDate,format="%a, %b %d %Y")
+	if(is.na(exptDate))
+		exptDate=strptime(x$vars$FileDate, format = "%a,%e %b %Y")
+	exptDateStr=format(exptDate)
+	
+	cat("date is:",exptDateStr)
+	
+	# Do we have a table representation of the data (in waves) ?
+	TableFields=sapply(x,function(x) !is.null(attr(x,"WaveHeader")))
+	dateFields=NULL
+	if(any(TableFields)){
+		d=x[TableFields]
+		xx=x[TableFields]
+	} else{
+		selFields=sapply(x[[2]]$vars,class)=="character"
+		d=do.call(rbind,lapply(x[-1],function(x) unlist(x$vars[selFields])))
+		dateFields=grep("[0-9][0-9]:[0-9][0-9]:[0-9][0-9]",as.character(d[1,]))
+		d=data.frame(d)		
+	}
+	#setdiff(names(x), grep("_[0-9]{3}",names(x),val=T))
+	
+	for (i in dateFields){
+		d[i]=as.POSIXct(paste(exptDateStr,as.character(d[,i])))
+#		d[i]=as.POSIXct(strptime(as.character(d[,i]),format="%H:%M:%S"))
+	}
+	d
+}
+
+ReadAllNclampLogTables<-function(logfiledir="/GD/projects/PhysiologyData/logs",...){
+	logfilenames=dir(logfiledir,full=T)
+	logfiles<<-list()
+	for(i in seq(logfilenames)){
+		if(i%%10>0) cat(".") else cat(as.integer(i))
+		cat("logfilenames[i]=",logfilenames[i],"\n")
+		logfiles[[i]]<<-try(ReadNclampLogTable(logfilenames[i]),...)
+	}
+	logfilenames
+}
