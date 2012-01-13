8749304b7f63bb5bf27d3b06f60ba5a42aa86fa0
Gregory Jefferis <jefferis@gmail.com>
2012-01-13 17:22:15 +0000

    Add tests for Igor v2/5 Binary Wave (.ibw) files

---
+++ inst/tests/test-ibw.R
@@ -0,0 +1,25 @@
+# TODO: Tests for reading of Igor Binary Wave (.ibw) files
+# 
+# Author: jefferis
+###############################################################################
+
+context("Verify reading of Igor pxp files")
+
+test_that("Read Igor v5 file", {
+      expect_that (w5<<-ReadIgorBinary('../igor/version5.ibw'),
+          is_a("numeric"),'read igor v5 wave without error')
+      expect_that(attr(w5,'BinHeader')$version,
+          equals(5))
+      expect_that(tsp.igorwave(w5),
+          equals(structure(c(0, 5, 1), .Names = c("start", "end", "frequency"))))
+      
+    })
+
+test_that("Read Igor v2 file", {
+      expect_that (w2<<-ReadIgorBinary('../igor/version2.ibw'),
+          is_a("numeric"),'read igor v2 wave without error')
+      expect_that(attr(w2,'BinHeader')$version,
+          equals(2))
+      expect_that(tsp.igorwave(w2),
+          equals(structure(c(0, 5, 1), .Names = c("start", "end", "frequency"))))
+    })
