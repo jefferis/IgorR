4db1575146122a9c29b9bc2ec11f33a4c5a8b9f2
Gregory Jefferis <jefferis@gmail.com>
2012-01-13 16:19:03 +0000

    Add testthat tests – for reading pxp files

---
+++ tests/test-all.R
@@ -0,0 +1,10 @@
+# Run all tests for IgorR package
+# 
+# Following basic example from Jeroen Ooms / Szilard ... useR meeting 
+# 
+# Author: jefferis
+###############################################################################
+
+library(testthat)
+library(IgorR)
+test_package("IgorR")
