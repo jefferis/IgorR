2b97ec43d3f8561e30e2139cdab5cd5e7c99ce6a
Gregory Jefferis <jefferis@gmail.com>
2024-08-04 10:05:33 +0100

    URL updates

--- R/IgorR-package.R
+++ R/IgorR-package.R
@@ -3,11 +3,11 @@
 #' This package provides function to read data from the Igor Pro data analysis
 #' program by Wavemetrics. The data formats supported are Igor packed experiment
 #' format (\code{.pxp}) and Igor binary wave (\code{.ibw}). See:
-#' \url{http://www.wavemetrics.com/} for details.
+#' \url{https://www.wavemetrics.com/} for details.
 #'
 #' It also includes some functions to load special pxp files produced by the
 #' Neuromatic/Nclamp packages for recording and analysing neuronal #data. See
-#' \url{http://www.neuromatic.thinkrandom.com/} for details.
+#' \url{https://github.com/SilverLabUCL/NeuroMatic} for details.
 #'
 #' Key functions in the package include \code{\link{read.ibw}},
 #' \code{\link{read.pxp}}.
